8cc8f038839357078f93a275cbc4b3d8fc0cc1e2
gethomast <thomas.geheb@gmail.com>
2026-03-02 21:58:16 +0100

    fix qr code scanner

--- src/GtKram.WebApp/wwwroot/js/qr-code-scanner.js
+++ src/GtKram.WebApp/wwwroot/js/qr-code-scanner.js
@@ -1,97 +1,62 @@
 ﻿class QrCodeScanner {
-
-    constructor(videoElement, scanCallback) {
-        this.videoElement = videoElement;
-        this.canvasElement = document.createElement('canvas');
-        this.canvasContext = this.canvasElement.getContext("2d", { alpha: false, willReadFrequently: true });
-        this.canvasContext.imageSmoothingEnabled = false;
-        this.scanCallback = scanCallback;
-
+    constructor({ video, canvas, onDetected, onError }) {
+        this.video = video;
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d", { willReadFrequently: true });
+        this.onDetectedCallback = onDetected;
+        this.onErrorCallback = onError;
         this.scanning = false;
-        this.requestFrameId = null;
-        this.nativeDetector = null;
+        this.stream = null;
     }
 
     async start() {
-        if (!navigator.mediaDevices) {
-            console.error("Camera not found.");
-            return false;
-        }
-
-        if ('BarcodeDetector' in window && (await BarcodeDetector.getSupportedFormats()).includes('qr_code')) {
-            this.nativeDetector = new BarcodeDetector({ formats: ['qr_code'] });
-        }
-
+        if (this.scanning) return;
         try {
-            const videoStream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: "environment" } });
-            this.videoElement.srcObject = videoStream;
-            this.play();
-            return true;
-        } catch (err) {
-            console.error("Accessing camera failed: " + err.message);
-            return false;
-        }
-    }
-
-    stop() {
-        this.scanning = false;
-
-        if (this.requestFrameId) {
-            cancelAnimationFrame(this.requestFrameId);
-            this.requestFrameId = null;
-        }
-
-        this.videoElement.pause();
-        const videoStream = this.videoElement.srcObject;
-        this.videoElement.srcObject = null;
-
-        if (videoStream) {
-            videoStream.getTracks().forEach(t => t.stop());
-        }
-    }
-
-    play() {
-        this.videoElement.play().then(() => {
+            this.stream = await navigator.mediaDevices.getUserMedia({
+                video: { facingMode: "environment" },
+            });
+            this.video.srcObject = this.stream;
+            await this.video.play();
+            this.canvas.width = this.video.videoWidth;
+            this.canvas.height = this.video.videoHeight;
             this.scanning = true;
-            this.scanLoop();
-        });
+            requestAnimationFrame(() => this.tick());
+        } catch (err) {
+            if (this.onErrorCallback) this.onErrorCallback(err);
+        }
     }
 
-
-    scanLoop() {
-        if (!this.scanning) {
-            return;
+    destroy() {
+        this.scanning = false;
+        if (this.stream) {
+            this.stream.getTracks().forEach((track) => track.stop());
+            this.stream = null;
         }
+        this.video.srcObject = null;
+    }
 
-        if (this.videoElement.readyState === HTMLMediaElement.HAVE_ENOUGH_DATA) {
-            this.canvasElement.width = this.videoElement.videoWidth;
-            this.canvasElement.height = this.videoElement.videoHeight;
-            this.canvasContext.drawImage(this.videoElement, 0, 0, this.canvasElement.width, this.canvasElement.height);
-            const imageData = this.canvasContext.getImageData(0, 0, this.canvasElement.width, this.canvasElement.height);
+    resume() {
+        if (this.scanning) return;
+        this.scanning = true;
+        requestAnimationFrame(() => this.tick());
+    }
 
-            if (this.nativeDetector) {
-                this.nativeDetector.detect(imageData).then(barCodes => {
-                    if (barCodes.length == 0) {
-                        this.scanLoop();
-                    } else {
-                        this.scanning = false;
-                        this.videoElement.pause();
-                        this.scanCallback(barCodes[0].rawValue);
-                    }
-                });
-                return;
-            }
-
-            const code = jsQR(imageData.data, imageData.width, imageData.height);
-            if (code) {
+    tick() {
+        if (!this.scanning) return;
+        if (this.video.readyState === this.video.HAVE_ENOUGH_DATA) {
+            this.canvas.width = this.video.videoWidth;
+            this.canvas.height = this.video.videoHeight;
+            this.ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
+            const imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+            const code = jsQR(imageData.data, imageData.width, imageData.height, {
+                inversionAttempts: "dontInvert",
+            });
+            if (code && code.data) {
                 this.scanning = false;
-                this.videoElement.pause();
-                this.scanCallback(code.data);
+                if (this.onDetectedCallback) this.onDetectedCallback(code.data);
                 return;
             }
         }
-
-        this.requestFrameId = requestAnimationFrame(() => this.scanLoop());
+        requestAnimationFrame(() => this.tick());
     }
 }
-
