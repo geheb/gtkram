edefa4b316181726ba0aebb99ed9b956a66ed962
gethomast <thomas.geheb@gmail.com>
2026-03-04 22:52:15 +0100

    csp fix

--- src/GtKram.Infrastructure/Security/SecurityHeadersMiddleware.cs
+++ src/GtKram.Infrastructure/Security/SecurityHeadersMiddleware.cs
@@ -8,25 +8,31 @@ public sealed class SecurityHeadersMiddleware
 {
     internal const string NonceKey = "CspNonce";
 
-    private static readonly string _cspTemplate;
-    private static readonly string _cspNonce;
+    private static readonly string _cspBeforeNonce;
+    private static readonly string _cspAfterNonce;
     private readonly RequestDelegate _next;
 
     static SecurityHeadersMiddleware()
     {
-        _cspNonce = Convert.ToBase64String(RandomNumberGenerator.GetBytes(16));
+        var before = new StringBuilder();
+        before.Append(GetDirective("default-src", "'self'"));
+        before.Append("script-src 'self' 'nonce-");
 
-        var directives = new StringBuilder();
-        directives.Append(GetDirective("default-src", "'self'"));
-        directives.Append(GetDirective("script-src", "'self'", $"'nonce-{_cspNonce}'"));
-        directives.Append(GetDirective("style-src", "'self'"));
-        directives.Append(GetDirective("img-src", "'self'", "data:"));
-        directives.Append(GetDirective("font-src", "'self'"));
-        directives.Append(GetDirective("media-src", "'self'"));
-        directives.Append(GetDirective("connect-src", "'self'"));
-        directives.Append(GetDirective("worker-src", "'self'"));
-        directives.Append(GetDirective("frame-ancestors", "'none'"));
-        _cspTemplate = directives.ToString();
+        var after = new StringBuilder();
+        after.Append("'; ");
+        after.Append(GetDirective("style-src", "'self'"));
+        after.Append(GetDirective("img-src", "'self'", "data:"));
+        after.Append(GetDirective("font-src", "'self'"));
+        after.Append(GetDirective("media-src", "'self'"));
+        after.Append(GetDirective("connect-src", "'self'"));
+        after.Append(GetDirective("worker-src", "'self'"));
+        after.Append(GetDirective("frame-ancestors", "'none'"));
+        after.Append(GetDirective("object-src", "'none'"));
+        after.Append(GetDirective("base-uri", "'self'"));
+        after.Append(GetDirective("form-action", "'self'"));
+
+        _cspBeforeNonce = before.ToString();
+        _cspAfterNonce = after.ToString();
     }
 
     public SecurityHeadersMiddleware(RequestDelegate next)
@@ -36,9 +42,11 @@ public sealed class SecurityHeadersMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        context.Items[NonceKey] = _cspNonce;
+        var nonce = Convert.ToBase64String(RandomNumberGenerator.GetBytes(16));
+        context.Items[NonceKey] = nonce;
+
         var headers = context.Response.Headers;
-        headers.ContentSecurityPolicy = _cspTemplate;
+        headers.ContentSecurityPolicy = string.Concat(_cspBeforeNonce, nonce, _cspAfterNonce);
         headers["X-Content-Type-Options"] = "nosniff";
         headers["X-Frame-Options"] = "DENY";
         headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
